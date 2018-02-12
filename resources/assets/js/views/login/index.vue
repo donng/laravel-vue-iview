785b32936694e2caf7d2afa4e31f3b73efa914b5
donng <xdong001@126.com>
2018-02-12 21:32:51 +0800

    登录页北京

--- resources/assets/js/views/login/index.vue
+++ resources/assets/js/views/login/index.vue
@@ -1,29 +1,34 @@
 <template>
-  <Row type="flex" justify="center" class-name="row">
-    <Col span="6">
-    <Card>
-      <p slot="title">Sign In</p>
-      <Form ref="form" :model="form" :rules="ruleInline">
-        <FormItem prop="email">
-          <Input type="text" v-model="form.email" placeholder="邮箱地址">
-          <Icon type="email" slot="prepend"></Icon>
-          </Input>
-        </FormItem>
-        <FormItem prop="password">
-          <Input type="password" v-model="form.password" placeholder="密码">
-          <Icon type="locked" slot="prepend"></Icon>
-          </Input>
-        </FormItem>
-        <FormItem>
-          <Button type="primary" @click="submit('form')" long>登录</Button>
-        </FormItem>
-      </Form>
-    </Card>
-    </Col>
-  </Row>
+  <div>
+    <div id="particles-js"></div>
+    <Row type="flex" justify="center" class-name="row">
+      <Col span="6">
+      <Card>
+        <p slot="title">Sign In</p>
+        <Form ref="form" :model="form" :rules="ruleInline">
+          <FormItem prop="email">
+            <Input type="text" v-model="form.email" placeholder="邮箱地址">
+            <Icon type="email" slot="prepend"></Icon>
+            </Input>
+          </FormItem>
+          <FormItem prop="password">
+            <Input type="password" v-model="form.password" placeholder="密码">
+            <Icon type="locked" slot="prepend"></Icon>
+            </Input>
+          </FormItem>
+          <FormItem>
+            <Button type="primary" @click="submit('form')" long>登录</Button>
+          </FormItem>
+        </Form>
+      </Card>
+      </Col>
+    </Row>
+  </div>
 </template>
 
 <script>
+  import particles from 'particles.js'
+
   export default {
     name: "index",
     data () {
@@ -53,6 +58,9 @@
           }
         })
       }
+    },
+    mounted () {
+      particlesJS.load('particles-js', 'particles.json');
     }
   }
 </script>
@@ -60,5 +68,15 @@
 <style scoped>
   .row {
     padding-top: 180px;
+  }
+
+  #particles-js {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background-color: #b61924;
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: 50% 50%;
   }
 </style>
