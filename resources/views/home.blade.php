89846bf712930f1bbed83cea6cff075a42b98de6
donng <xdong001@126.com>
2018-03-01 20:10:59 +0800

    1

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,7 +13,6 @@
                             {{ session('status') }}
                         </div>
                     @endif
-
                     You are logged in!
                 </div>
             </div>
